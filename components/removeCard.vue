d44586dff1b586011ea4c9f5f72ece581b93dec1
도다 (@ddarkr) <ddark.kr@gmail.com>
2020-04-28 18:47:28 +0900

    feat(removeCard): make removeCard

---
+++ components/removeCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <seciton>
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">
+          Copy Github repository labels to another repository
+        </p>
+      </header>
+      <div class="card-content">
+        <div class="content">
+          <b-field label="Name">
+            <b-input value="Kevin Garvey"></b-input>
+          </b-field>
+        </div>
+      </div>
+    </div>
+  </seciton>
+</template>
+
+<script>
+export default {
+  methods: {
+    action() {}
+  }
+}
+</script>
